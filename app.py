97dbf9daec66f317f4f1facbbb1236cab08c1700
Michael Stevenson <503stevenson@gmail.com>
2023-03-13 16:26:45 -0400

    new route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def home():
     return render_template('home.html')
 
+@app.route("/create")
+def create():
+    return render_template('create.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
