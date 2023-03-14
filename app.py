3e728dc0514de587a06774ec9fffe83108e9d21f
Michael Stevenson <503stevenson@gmail.com>
2023-03-14 00:11:21 -0400

    template changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 from database import load_all_dishes
 
 app = Flask(__name__)
@@ -15,6 +15,12 @@ def create():
 def explore():
     dishes = load_all_dishes()
     return render_template('explore.html', dishes=dishes)
+
+#json page to view dishes
+@app.route("/api/dishes")
+def list_dishes():
+    dishes = load_all_dishes()
+    return jsonify(dishes)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
