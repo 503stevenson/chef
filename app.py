5dc62b7cdfd407e04ba3df63fb8f07add384f1c4
Michael Stevenson <503stevenson@gmail.com>
2023-03-14 17:15:38 -0400

    add new dishes to database from create form.

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
-from flask import Flask, render_template, jsonify
-from database import load_all_dishes
+from flask import Flask, flash, redirect, render_template, jsonify, request
+from database import load_all_dishes, add_dish
 
+#variables
+formStepOne = False
+Ingredientdata = False
+error = False
+
+#app
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,7 +15,39 @@ def home():
 
 @app.route("/create")
 def create():
-    return render_template('create.html')
+    global error
+    if error:
+        error = False
+        return render_template('create.html', error=True)
+    else:
+        return render_template('create.html', error=False)
+
+@app.route("/create/ingredients", methods=['post'])
+def postIngredients():
+    global formStepOne
+    global Ingredientdata
+    formStepOne = True
+    Ingredientdata = request.form.to_dict()
+    return render_template('formTwo.html')
+
+@app.route("/create/details", methods=['post'])
+def postDish():
+    global Ingredientdata
+    global formStepOne
+    global error
+    newDish = request.form.to_dict()
+
+    if Ingredientdata != False and Ingredientdata != None and formStepOne:
+        newDish['ingredients'] = Ingredientdata['ingredients']
+        newDish['directions'] = Ingredientdata['directions']
+        Ingredientdata = False
+        formStepOne = False
+        add_dish(newDish)
+        return jsonify(newDish)
+    Ingredientdata = False
+    formStepOne = False
+    error = True
+    return redirect("/create")
 
 @app.route("/explore")
 def explore():
--- database.py
+++ database.py
@@ -12,7 +12,28 @@ def load_all_dishes():
     with engine.connect() as conn:
         result=conn.execute(text('select * from dishes'))
 
-        records = []    
+        records = []
+        mapping = {0: 'id', 1: 'dishName', 2: 'cookTime', 3: 'country', 4: 'ingredients', 5: 'directions', 6: 'image'} 
         for row in result.all():
-            records.append(dict(row))
+            i = 0
+            record = {}
+            for item in row:
+                record[mapping[i]] = item
+                i+= 1
+            records.append(record)
     return records
+
+def add_dish(dish):
+    with engine.connect() as conn:
+        dishName=dish['name']
+        cookTime=dish['time']
+        country=dish['country']
+        ingredients=dish['ingredients']
+        directions=dish['directions']
+        image=dish['image']
+        values = '\'' + dishName + '\', \'' + cookTime + '\', \'' + country + '\', \'' + ingredients + '\', \'' + directions + '\''
+        query = text("INSERT INTO dishes(dishName, cookTime, country, ingredients, directions) VALUES(" + values + ")")
+
+        conn.execute(query)
+    
+print(load_all_dishes())
